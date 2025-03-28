b526937834cb101cd2c5c44d00104b4dd765affa
wallenjos01 <wallenjos01@gmail.com>
2025-03-28 16:56:37 -0600

    Use release versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,5 +16,5 @@ dependencies {
     compileOnly(libs.jetbrains.annotations)
 
     modImplementation("org.wallentines:databridge:0.6.0")
-    modImplementation("org.wallentines:pseudonym-minecraft:0.2.0-SNAPSHOT")
+    modImplementation("org.wallentines:pseudonym-minecraft:0.2.0")
 }
